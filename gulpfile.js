0f579dfe5f0f3cdd5c2db6de164d137dc57f3124
Thirumuruga <thirumurugan3131@gmail.com>
2019-01-25 19:36:16 +0530

    configurations added

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const ifElse = require("gulp-if-else");
+const argv = require("yargs").argv;
+
+gulp.task("images", () => {
+  gulp
+    .src(["src/images/**/*.{jpg,jpeg,png,gif}"], { base: "src/images" })
+    .pipe(ifElse(argv.compress, () => imagemin()))
+    .pipe(gulp.dest("build/images"));
+});
